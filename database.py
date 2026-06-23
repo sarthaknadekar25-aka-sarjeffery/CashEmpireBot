c98defd316ba042f7a45b768efc84b189420cc07
sarthaknadekar25-aka-sarjeffery <sarthaknadekar25@gmail.com>
2026-06-23 17:16:30 +0530

    Switch to PostgreSQL via DATABASE_URL

--- database.py
+++ database.py
@@ -1,22 +1,40 @@
 import os
 import json
+import threading
 
 DATA_FILE = "data/economy.json"
 
-mongo = None
-if os.getenv("MONGO_URI"):
-    from pymongo import MongoClient
-    mongo = MongoClient(os.getenv("MONGO_URI"))
-    db = mongo.get_database("cashempire")
-    col = db["players"]
+_db_lock = threading.Lock()
+_db_conn = None
+_db_ready = False
+
+DATABASE_URL = os.getenv("DATABASE_URL", "")
+
+if DATABASE_URL and DATABASE_URL.startswith("postgres"):
+    import psycopg2
+    import psycopg2.extras
+
+    _db_conn = psycopg2.connect(DATABASE_URL)
+    _db_conn.autocommit = True
+    cur = _db_conn.cursor()
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS players (
+            uid TEXT PRIMARY KEY,
+            data JSONB NOT NULL DEFAULT '{}'
+        )
+    """)
+    cur.close()
+    _db_ready = True
 
 
 def load_data():
-    if mongo:
-        data = {}
-        for doc in col.find():
-            data[doc["_id"]] = {k: v for k, v in doc.items() if k != "_id"}
-        return data
+    if _db_ready:
+        with _db_lock:
+            cur = _db_conn.cursor()
+            cur.execute("SELECT uid, data FROM players")
+            rows = cur.fetchall()
+            cur.close()
+            return {row[0]: dict(row[1]) for row in rows}
     if not os.path.exists("data"):
         os.makedirs("data")
     if os.path.exists(DATA_FILE):
@@ -26,10 +44,17 @@ def load_data():
 
 
 def save_data(data):
-    if mongo:
-        col.delete_many({})
-        if data:
-            col.insert_many([{"_id": uid, **pd} for uid, pd in data.items()])
+    if _db_ready:
+        with _db_lock:
+            cur = _db_conn.cursor()
+            cur.execute("DELETE FROM players")
+            if data:
+                psycopg2.extras.execute_values(
+                    cur,
+                    "INSERT INTO players (uid, data) VALUES %s",
+                    [(uid, json.dumps(pd)) for uid, pd in data.items()]
+                )
+            cur.close()
         return
     with open(DATA_FILE, "w") as f:
         json.dump(data, f, indent=2)
